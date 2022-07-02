0169bc4e88432306edb56b190c7c074e904af2d2
Khanaphon "kana" Phaengtan <khwd.2007@gmail.com>
2022-07-03 04:33:22 +0700

    fix(core): wait... what?!

--- src/run.ts
+++ src/run.ts
@@ -19,7 +19,7 @@ export const run = async (input: Inputs): Promise<void> => {
     })
     const filteredFiles = files.filter((i) => i.indexOf('.git/') === -1).filter((i) => i.indexOf('.github/') === -1)
     const lists = filteredFiles.map((i) => path.join(filePath, i))
-    const promises = Promise.all(
+    const promises = await Promise.all(
       lists.map(async (i) => {
         //Check if file size not zero and less than 5MB
         const stats = await fs.stat(i)
