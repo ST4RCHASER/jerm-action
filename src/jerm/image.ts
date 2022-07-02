7bdeb28b7da27fa18d0d6f883c8d607a5edb9050
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2022-07-03 01:47:44 +0700

    Update run.ts

--- src/jerm/image.ts
+++ src/jerm/image.ts
@@ -1,37 +1,17 @@
-import sharp from 'sharp'
 import fs from 'fs';
-export const compositeYan = async (imgSrc: string, yanSrc: string): Promise<sharp.OutputInfo> =>
-  {
-    const fileExtension = imgSrc.split('.').pop();
-    const img = sharp(imgSrc)
-    .resize(1024)
-    .composite([
-      {
-        input: yanSrc,
-        gravity: 'southeast',
-      },
-    ]);
-    switch (fileExtension) {
-      case 'jepg':
-      case 'jpg':
-        img.jpeg();
-        break;
-      case 'png':
-        img.png();
-        break;
-      case 'gif':
-        img.gif();
-        break;
-    }
-    const result = await img.toFile(imgSrc+'.yan');
-    //Delete old file and rename new file
-    fs.unlink(imgSrc, (err) => {
-      if (err) throw err;
-      fs.rename(imgSrc+'.yan', imgSrc, (err) => {
-        if (err) throw err;
-      }
-      )
-    }
-    )
-    return result;
-  }
+import ffmpeg from 'ffmpeg';
+import Jimp from 'jimp/*';
+export const compositeYan = async (imgSrc: string, yanSrc: string) => {
+  const image = await Jimp.read(imgSrc);
+  const yan = await Jimp.read(yanSrc);
+  
+  const xMargin = (image.bitmap.width * 5) / 100;
+  const yMargin = (image.bitmap.width * 5) / 100;
+
+  const X = image.bitmap.width - yan.bitmap.width - xMargin;
+  const Y = image.bitmap.height - yan.bitmap.height - yMargin;
+
+  const img =  await image.composite(yan, X, Y);
+  await img.write(imgSrc);
+  return img;
+}
