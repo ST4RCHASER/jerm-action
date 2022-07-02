0e5666d8d2ae546337b1fd041e68b216c258142d
Khanaphon "kana" Phaengtan <khwd.2007@gmail.com>
2022-07-02 14:46:50 +0700

    fix: 1

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  await run()
+  await run({ path: core.getInput('path') })
 }
 
 main().catch((e) => core.setFailed(e instanceof Error ? e.message : JSON.stringify(e)))
--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,11 @@
 import * as core from '@actions/core'
 import { promises as fs } from 'fs'
 type Inputs = {
-  name: string
+  path: string
 }
 
 // eslint-disable-next-line @typescript-eslint/require-await
-export const run = async (): Promise<void> => {
+export const run = async (input: Inputs): Promise<void> => {
   try {
     const files = await fs.readdir('./')
     core.setOutput('files', files.join(', '))
