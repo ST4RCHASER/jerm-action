2443bcfde98d412a384653189c16b823f8f58697
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2022-07-02 20:34:06 +0700

    Revert "fix: eslint!!!!"
    This reverts commit 8b7e4330ba2d730d9de7022b7713240c9b0bba21.

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import { promises as fs } from 'fs'
 import walk from 'ignore-walk'
 import path from 'path'
 type Inputs = {
@@ -7,12 +8,16 @@ type Inputs = {
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const run = async (input: Inputs): Promise<void> => {
-  const filePath = path.resolve(input.path)
-  const files = await walk({
-    path: filePath,
-    includeEmpty: false,
-    ignoreFiles: ['.gitignore', '.prettierignore'],
-  })
-  const filteredFiles = files.filter((i) => i.indexOf('.git/') === -1).filter((i) => i.indexOf('.github/') === -1)
-  core.info(`Results: ${JSON.stringify(filteredFiles.map((i) => path.join(filePath, i)))}`)
+  try {
+    const filePath = path.resolve(input.path)
+    const files = await walk({
+      path: filePath,
+      includeEmpty: false,
+      ignoreFiles: ['.gitignore', '.prettierignore'],
+    })
+    let filteredFiles = files.filter((i) => i.indexOf('.git/') === -1).filter((i) => i.indexOf('.github/') === -1)
+    core.info(`Results: ${JSON.stringify(filteredFiles.map((i) => path.join(filePath, i)))}`)
+  } catch (err) {
+    core.error(`sumting wrong with something: ${err}`)
+  }
 }
