1d3fec8f476c098bb03d8f46fac3abef74b30758
Khanaphon "kana" Phaengtan <khwd.2007@gmail.com>
2022-07-03 04:24:06 +0700

    feat(ci): more logging

--- src/jerm/ascii.ts
+++ src/jerm/ascii.ts
@@ -1,10 +1,12 @@
-import fs from 'fs'
+import * as core from '@actions/core'
+import { promises as fs } from 'fs'
 export const writeText = async (path: string) => {
-    //Add text to first of line
-    const text = "==========TEST=========="
-    const content = await fs.readFileSync(path, 'utf8');
-    const lines = content.split('\n');
-    const firstLine = lines[0];
-    const newContent = `${text}\n${firstLine}`;
-    return await fs.writeFileSync(path, newContent, 'utf8');
+  //Add text to first of line
+  const text = '==========TEST=========='
+  const content = await fs.readFile(path, 'utf8')
+  const lines = content.split('\n')
+  const firstLine = lines[0]
+  const newContent = `${text}\n${firstLine}`
+  core.info(`Writing text to first line of file: ${path}`)
+  return fs.writeFile(path, newContent, 'utf8')
 }
