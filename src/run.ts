b9c86d066abf6029e6284bb333dd5ce603152fc5
Khanaphon "kana" Phaengtan <khwd.2007@gmail.com>
2022-07-02 15:20:42 +0700

    feat: recursively walk path...

--- src/run.ts
+++ src/run.ts
@@ -9,6 +9,24 @@ export const run = async (input: Inputs): Promise<void> => {
   try {
     const files = await fs.readdir('./')
     core.info(`Files: ${JSON.stringify(files)}`)
+    core.info(`pwd: ${process.cwd()}`)
+
+    //recursively walk the directory
+    const walk = async (dir: string): Promise<void> => {
+      const files = await fs.readdir(dir)
+      for (const file of files) {
+        const path = `${dir}/${file}`
+        const stat = await fs.stat(path)
+        if (stat.isDirectory()) {
+          await walk(path)
+        } else {
+          core.info(`${path}`)
+        }
+      }
+    }
+    await walk(input.path)
+
+
     // core.setOutput('files', files.join(', '))
   } catch (err) {
     console.error(err)
