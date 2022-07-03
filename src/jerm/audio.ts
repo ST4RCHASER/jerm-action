7f08afe5ff370f614fd0ca2bfd8ec3748a971721
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2022-07-03 09:53:20 +0700

    Update audio.ts

--- src/jerm/audio.ts
+++ src/jerm/audio.ts
@@ -5,34 +5,29 @@ import * as core from '@actions/core'
 /* eslint-disable @typescript-eslint/no-var-requires */
 const audioconcat = require('audioconcat')
 import fs from 'fs';
-import { exec } from 'child_process';
 export const concatYanAudio = (audioSrc: string, yarnSrc: string) => {
-  return new Promise<void>((resolve, reject) => {
-    //Run ffmpeg command
-    exec(`ffmpeg -i "concat:${audioSrc}|${yarnSrc}" -acodec copy ${audioSrc}-yan.mp3`, (err, stdout, stderr) => {
-      // exec(`ffmpeg -i ${audioSrc} -i ${yarnSrc} -filter_complex "[1]adelay=Ns|Ns[a1];[0:a][a1]amix=inputs=2[a]" -map "[a]" ${audioSrc}-yan.mp3`, (err, stdout, stderr) => {
-      // exec(`ffmpeg -i ${audioSrc} -i ${yarnSrc} -filter_complex "[0][1]amerge=inputs=2,pan=stereo|FL<c0+c1|FR<c2+c3[a]" -map "[a]" ${audioSrc}-yan.mp3`, (err, stdout, stderr) => {
-      if (err) {
-        core.error(`Error: ${err}`)
-        reject(err)
-      } else {
-        core.info(`stdout: ${stdout}`)
-        //delete old and rename new file
-        fs.unlink(audioSrc, (err) => {
-          if (err) {
-            core.error(`Error: ${err}`)
-            reject(err)
-          } else {
-            fs.rename(`${audioSrc}-yan.mp3`, audioSrc, (err) => {
-              if (err) {
-                core.error(`Error: ${err}`)
-                reject(err)
-              }
-              resolve();
-            })
-          }
-        });
-      }
-    });
-  });
+return new Promise<void>((resolve, reject) => {
+  audioconcat([yarnSrc, audioSrc])
+    .concat(`${audioSrc}-yan.mp3`)
+    .on('start', function (command: unknown) {
+      core.info(`ffmpeg process started: ${command as string}`)
+    })
+    .on('error', function (err: unknown, stdout: unknown, stderr: unknown) {
+      core.error(`Error: ${err as string}`)
+      core.error(`ffmpeg stderr: ${stderr as string}`)
+      reject(err)
+    })
+    .on('end', function (output: unknown) {
+      core.error(`Audio created in: ${output as string}`)
+      //delete old and rename new file
+      fs.unlink(audioSrc, (err) => {
+        if (err) throw err
+        fs.rename(`${audioSrc}-yan.mp3`, audioSrc, (err) => {
+          if (err) throw err
+          resolve();
+        }
+        )
+      });
+    })
+});
 }
