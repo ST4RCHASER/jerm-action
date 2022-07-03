8adf5555323ab79f212572ab21bd79d372bf1a54
_StarChaser <32775520+ST4RCHASER@users.noreply.github.com>
2022-07-03 12:22:06 +0700

    Update video.ts

--- src/jerm/video.ts
+++ src/jerm/video.ts
@@ -3,43 +3,33 @@ import * as core from '@actions/core'
 import fs from 'fs';
 const mergeVideo = async (filePath: string, imagePath: string, audioSrc: string): Promise<void> => {
     return new Promise<void>((resolve, reject) => {
-        let result = exec(`ffmpeg -i ${filePath} -i ${imagePath} -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -codec:a copy ${filePath}-yan.mp4`, (err) => {
+        let result = exec(`ffmpeg -i ${filePath} -i ${imagePath} -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -codec:a copy ${filePath}-yanad.mp4`, (err) => {
             if (err) {
                 reject(err);
             }
-            //delete old and rename new file
-            fs.unlink(filePath, (err) => {
+            exec(`ffmpeg -i ${filePath}-yanad.mp4 -i ${audioSrc} -filter_complex "[0][1]amerge=inputs=2,pan=stereo|FL<c0+c1|FR<c2+c3[a]" -map "[a]" ${audioSrc}-yanfinish.mp4`, (err, stdout, stderr) => {
                 if (err) {
-                    reject(err);
-                }
-                fs.rename(`${filePath}-yan.mp4`, filePath, (err) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    exec(`ffmpeg -i ${filePath} -i ${audioSrc} -filter_complex "[0][1]amerge=inputs=2,pan=stereo|FL<c0+c1|FR<c2+c3[a]" -map "[a]" ${audioSrc}-yan.mp4`, (err, stdout, stderr) => {
+                    core.error(`Error: ${err}`)
+                    reject(err)
+                } else {
+                    core.info(`stdout: ${stdout}`)
+                    //delete old and rename new file
+                    fs.unlinkSync(`${filePath}-yanad.mp4`);
+                    fs.unlink(filePath, (err) => {
                         if (err) {
-                          core.error(`Error: ${err}`)
-                          reject(err)
+                            core.error(`Error: ${err}`)
+                            reject(err)
                         } else {
-                          core.info(`stdout: ${stdout}`)
-                          //delete old and rename new file
-                          fs.unlink(filePath, (err) => {
-                            if (err) {
-                              core.error(`Error: ${err}`)
-                              reject(err)
-                            } else {
-                              fs.rename(`${filePath}-yan.mp4`, filePath, (err) => {
+                            fs.rename(`${filePath}-yanfinish.mp4`, filePath, (err) => {
                                 if (err) {
-                                  core.error(`Error: ${err}`)
-                                  reject(err)
+                                    core.error(`Error: ${err}`)
+                                    reject(err)
                                 }
                                 resolve();
-                              })
-                            }
-                          });
+                            })
                         }
-                      });
-                }                );
+                    });
+                }
             });
         });
     });
