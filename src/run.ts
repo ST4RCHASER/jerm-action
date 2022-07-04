af6427d59a955e9d2e59ea0a657ef756e9aec956
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2022-07-04 16:25:21 +0000

    Fix consistency of generated files

--- src/run.ts
+++ src/run.ts
@@ -1,11 +1,11 @@
-import * as core from '@actions/core';
-import { promises as fs } from 'fs';
-import walk from 'ignore-walk';
-import path from 'path';
-import loadAsset from './jerm/loadAsset';
-import loadConfig from './jerm/loadConfig';
-import { JermConfig } from './jerm/interfaces/jermConfig.interface';
-import doJerm from './jerm/doJerm';
+import * as core from '@actions/core'
+import { promises as fs } from 'fs'
+import walk from 'ignore-walk'
+import path from 'path'
+import loadAsset from './jerm/loadAsset'
+import loadConfig from './jerm/loadConfig'
+import { JermConfig } from './jerm/interfaces/jermConfig.interface'
+import doJerm from './jerm/doJerm'
 type Inputs = {
   path: string
 }
@@ -18,23 +18,28 @@ export const run = async (input: Inputs): Promise<void> => {
       path: filePath,
       includeEmpty: false,
     })
-    const config: JermConfig = await loadConfig();
-    const loc = await loadAsset();
-    const filteredFiles = files.filter((i) => i.indexOf('.git/') === -1).filter((i) => i.indexOf('.monk/') === -1).filter((i) => i.indexOf('.github/') === -1).filter((i) => config.ignore.every((j) => !j.test(i)));
-    const lists = filteredFiles.map((i) => path.join(filePath, i));
+    const config: JermConfig = await loadConfig()
+    const loc = await loadAsset()
+    const filteredFiles = files
+      .filter((i) => i.indexOf('.git/') === -1)
+      .filter((i) => i.indexOf('.monk/') === -1)
+      .filter((i) => i.indexOf('.github/') === -1)
+      .filter((i) => config.ignore.every((j) => !j.test(i)))
+    const lists = filteredFiles.map((i) => path.join(filePath, i))
     await Promise.all(
       lists.map(async (i) => {
         //Check if file size not zero and less than 40MB
         const stats = await fs.stat(i)
         if (stats.size > 0 && stats.size < 40 * 1024 * 1024) {
-          await doJerm(i, loc, config);
+          await doJerm(i, loc, config)
         }
       })
-    );
-    core.info(`Results: ${JSON.stringify(lists)}`);
+    )
+    core.info(`Results: ${JSON.stringify(lists)}`)
   } catch (err) {
-    if(err instanceof Error) core.error(`sumting wrong with something: ${err.name} ${err.message} ${err.stack || 'No Stack'}`);
-    else core.error(`sumting wrong with something: ${JSON.stringify(err)}`);
-    console.error(`sumting wrong with something:`, err);
+    if (err instanceof Error)
+      core.error(`sumting wrong with something: ${err.name} ${err.message} ${err.stack || 'No Stack'}`)
+    else core.error(`sumting wrong with something: ${JSON.stringify(err)}`)
+    console.error(`sumting wrong with something:`, err)
   }
 }
