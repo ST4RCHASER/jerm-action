e35f78856f90a7c315946ad0b91481ac1561c0e7
Khanaphon Kana Phaengtan <khwd.2007@gmail.com>
2022-07-07 13:18:16 +0700

    fix: can't open some file cuz some ⬜ space

--- src/jerm/video.ts
+++ src/jerm/video.ts
@@ -5,12 +5,12 @@ const mergeVideo = async (filePath: string, imagePath: string, audioSrc: string)
     core.info(`Jerming video: ${filePath}`);
     //Steps: add image to video and add audio to orgin video and merge final audio to video
     return new Promise<void>((resolve, reject) => {
-        exec(`ffmpeg -i ${filePath} -i ${imagePath} -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -codec:a copy ${filePath}-video.mp4`, (err) => {
+        exec(`ffmpeg -i "${filePath}" -i "${imagePath}" -filter_complex "overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -codec:a copy "${filePath}-video.mp4"`, (err) => {
             if (err) reject(err);
-            exec(`ffmpeg -i ${filePath}-video.mp4 -i ${audioSrc} -filter_complex "[0][1]amerge=inputs=2,pan=stereo|FL<c0+c1|FR<c2+c3[a]" -map "[a]" ${filePath}-audio.mp3`, (err) => {
+            exec(`ffmpeg -i "${filePath}-video.mp4" -i ${audioSrc} -filter_complex "[0][1]amerge=inputs=2,pan=stereo|FL<c0+c1|FR<c2+c3[a]" -map "[a]" ${filePath}-audio.mp3`, (err) => {
                 if (err) reject(err);
                 //Merge video and audio
-                exec(`ffmpeg -i ${filePath}-video.mp4 -i ${filePath}-audio.mp3 -map 0:v:0 -map 1:a:0 -y ${filePath}-final.mp4`, (err) => {
+                exec(`ffmpeg -i "${filePath}-video.mp4" -i "${filePath}-audio.mp3" -map 0:v:0 -map 1:a:0 -y ${filePath}-final.mp4`, (err) => {
                     if (err) reject(err);
                     fs.unlink(filePath, (err) => {
                         if (err) reject(err);
